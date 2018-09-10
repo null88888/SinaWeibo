a344d2144cf27edb375815d79b761153616d8ff4
ljw <ljw@ljwdemacbook-air.local>
2018-09-10 17:03:17 +0800

    extension tableView的代理方法

--- 新浪微博/新浪微博/Classes/View/Main/WBBaseViewController.swift
+++ 新浪微博/新浪微博/Classes/View/Main/WBBaseViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+//Swift中使用协议类似于多继承
+//class WBBaseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 class WBBaseViewController: UIViewController {
 
     var tableView: UITableView?
@@ -43,6 +44,8 @@ extension WBBaseViewController {
     func setupTableView() {
         tableView = UITableView(frame: view.bounds, style: .plain)
         view.insertSubview(tableView!, belowSubview: navigationBar)
+        tableView?.delegate = self;
+        tableView?.dataSource = self;
     }
     
     func setupNavigationBar() {
@@ -51,5 +54,19 @@ extension WBBaseViewController {
         navigationBar.items = [navItem]
         navigationBar.barTintColor = UIColor.cz_color(withHex: 0xF6F6F6)
         navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.darkGray]
+    }
+}
+
+//MARK: - UITableViewDataSource, UITableViewDelegate
+extension WBBaseViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    //基类只是准备方法，子类负责具体的实现
+    //子类的数据源方法不需要super
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
