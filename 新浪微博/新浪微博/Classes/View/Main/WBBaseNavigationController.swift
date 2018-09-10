665e301ad0cb9a40485110a4542e2ab524992d31
ljw <ljw@ljwdemacbook-air.local>
2018-09-10 11:02:57 +0800

    设置导航栏返回按钮

--- 新浪微博/新浪微博/Classes/View/Main/WBBaseNavigationController.swift
+++ 新浪微博/新浪微博/Classes/View/Main/WBBaseNavigationController.swift
@@ -22,11 +22,29 @@ class WBBaseNavigationController: UINavigationController {
         
         //为根视图时 childViewControllers.count == 0
         //当不是跟视图时才隐藏tabBar
+        //viewController 是被push的控制器，设置他的左侧按钮作为返回按钮
         if childViewControllers.count > 0 {
             viewController.hidesBottomBarWhenPushed = true
+            
+            //判断控制器的类型
+            if let vc = viewController as? WBBaseViewController {
+                var title = "返回"
+                
+                if childViewControllers.count == 1 {
+                    title = childViewControllers.first?.title ?? "返回"
+                }
+                
+                //取出自定义的 navItem
+                vc.navItem.leftBarButtonItem = UIBarButtonItem(title: title, target: self, action: #selector(popToParent))
+            }
         }
         
         super.pushViewController(viewController, animated: animated)
+    }
+    
+    //POP返回上一级控制器
+    @objc private func popToParent() {
+        popViewController(animated: true)
     }
 
 }
